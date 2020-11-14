f494c3baf8354f031f8eceb4b110b62db1a02dd6
Nicolas Justin <nicolasjustin.dev@gmail.com>
2020-11-14 18:53:42 -0300

    add: support to storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,16 +1,17 @@
 import * as S from './styles';
 
-export default function Main() {
+export default function Main({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components',
+}) {
   return (
     <S.Wrapper>
       <S.Logo
         src="/img/logo.svg"
         alt="Imagem de um átomo com a palavra React Avançado escrito ao seu lado direito."
       />
-      <S.Title>React Avançado</S.Title>
-      <S.Description>
-        TypeScript, ReactJS, NextJS e Styled Components
-      </S.Description>
+      <S.Title>{title}</S.Title>
+      <S.Description>{description}</S.Description>
       <S.Illustration
         src="/img/hero-illustration.svg"
         alt="Imagem de um desenvolvedor sentado em frente a um computador."
---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,19 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import Main from '.';
+
+export default {
+  title: 'Main',
+  component: Main,
+  args: {
+    title: 'Nulla porttitor accumsan',
+    description: 'Vivamus suscipit tortor eget felis porttitor volutpat.',
+  },
+} as Meta;
+
+export const Basic: Story = (args) => <Main {...args} />;
+
+Basic.args = {
+  title: 'React Avançado',
+  description: 'TypeScript, ReactJS, NextJS e Styled Components',
+};
