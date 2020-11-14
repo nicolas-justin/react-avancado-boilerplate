abfee3325028f39ef611518ac4d20bb081f97af6
Nicolas Justin <nicolasjustin.dev@gmail.com>
2020-11-14 15:53:29 -0300

    add: support to global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,22 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-import '../../styles/globals.css';
+import GlobalStyles from 'styles/global';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Boilerplate | React Avançado</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple project start to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  *,
+  ::before,
+  ::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-style: 62.5%;
+  }
+
+  body {
+    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  html,
+  body,
+  #__next {
+    height: 100%;
+  }
+`;
+
+export default GlobalStyles;
