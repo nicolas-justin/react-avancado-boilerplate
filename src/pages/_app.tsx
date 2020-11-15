bfed89c50146f19157fbd35979f708c718155b98
Nicolas Justin <nicolasjustin.dev@gmail.com>
2020-11-14 22:37:43 -0300

    add: support to pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <title>Boilerplate | React Avançado</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple project start to work with TypeScript, React, NextJS and Styled Components"
