32338ba7a32f2fdd97a64007288a3eb2ef9daacc
Nicolas Justin <nicolasjustin.dev@gmail.com>
2020-11-15 09:38:33 -0300

    update: jest config to cover files with .ts extension

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testEnvironment: 'jsdom',
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   testPathIgnorePatterns: ['/node_modules', '/.next'],
 };
