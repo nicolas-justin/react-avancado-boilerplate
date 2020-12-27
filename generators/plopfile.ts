31e24871547783df15f2f39c2a9bffa83c98fbda
Nicolas Justin <nicolasjustin.dev@gmail.com>
2020-12-27 20:43:48 -0300

    refactor(plopfile): migrate to typescript

---
+++ generators/plopfile.ts
@@ -0,0 +1,38 @@
+import { NodePlopAPI } from 'plop';
+
+const componentConfig = {
+  description: 'application component logic',
+  prompts: [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'component name please',
+    },
+  ],
+  actions: [
+    {
+      type: 'add',
+      path: '../src/components/{{pascalCase name}}/index.tsx',
+      templateFile: 'templates/index.tsx.hbs',
+    },
+    {
+      type: 'add',
+      path: '../src/components/{{pascalCase name}}/styles.ts',
+      templateFile: 'templates/styles.ts.hbs',
+    },
+    {
+      type: 'add',
+      path: '../src/components/{{pascalCase name}}/stories.tsx',
+      templateFile: 'templates/stories.tsx.hbs',
+    },
+    {
+      type: 'add',
+      path: '../src/components/{{pascalCase name}}/test.tsx',
+      templateFile: 'templates/test.tsx.hbs',
+    },
+  ],
+};
+
+export default (plop: NodePlopAPI) => {
+  plop.setGenerator('component', componentConfig);
+};
